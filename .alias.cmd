4c6e7aefaa4c257a1690127cdd5f448e360b5a09
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2025-03-03 17:08:29 +1100

    Add Git related aliases.

--- .alias.cmd
+++ .alias.cmd
@@ -6,18 +6,37 @@
 @REM   or %USERPROFILE%\Dotfiles\.alias.cmd
 @REM Just try `doskey /?`
 
-doskey ls=ls -F --color $*
-doskey ll=ls -F --color -lh $*
-doskey lt=ls -F --color -tr $*
-doskey llt=ls -F --color -lhtr $*
-doskey llta=ls -F --color -lhtra $*
-doskey lld=ls -F --color -lhd $*
-doskey la=ls -F --color -lha $*
+@REM
+@REM `ls` aliases
+@REM
+@REM Cannot insert whitespaces before and after `=` in the following lines.
+doskey      ls=ls -F --color $*
+doskey      ll=ls -F --color -lh $*
+doskey      lt=ls -F --color -tr $*
+doskey     llt=ls -F --color -lhtr $*
+doskey    llta=ls -F --color -lhtra $*
+doskey     lld=ls -F --color -lhd $*
+doskey      la=ls -F --color -lha $*
 
 doskey vi=vim $*
 doskey mv=mv --interactive $*
 doskey rm=rm --interactive $*
 doskey cp=cp --interactive $*
+
+@REM
+@REM Git aliases
+@REM
+doskey   gst=git status $*
+doskey gdiff=git diff $*
+doskey   gbr=git branch $*
+doskey  gadd=git add $*
+doskey   gci=git commit -m $*
+doskey   gco=git checkout $*
+doskey   gps=git push $*
+doskey   gpl=git pull $*
+doskey    gf=git fetch $*
+doskey  glog=git log $*
+doskey gshow=git show $*
 
 @REM Set a system environment variable, LESS, to --ignore-case, which would be better than following.
 @REM doskey less=less --ignore-case $*
